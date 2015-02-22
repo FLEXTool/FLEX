cdb9235a83d9e4434f15ea7c3685f729c301278f
Ryan Olson <ryanolsonk@gmail.com>
2015-02-21 16:57:11 -0800

    Add network debugging settings view controller

---
+++ Classes/Network/FLEXNetworkSettingsTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  FLEXNetworkSettingsTableViewController.h
+//  FLEXInjected
+//
+//  Created by Ryan Olson on 2/20/15.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FLEXNetworkSettingsTableViewController : UITableViewController
+
+@end
