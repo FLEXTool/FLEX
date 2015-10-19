7f28d430d0e0f447aab8703910db4379afc503b3
Rich Robinson <robinsonrc@gmail.com>
2015-10-19 20:05:04 +0100

    Add basic support for viewing shared HTTP cookie storage

---
+++ Classes/GlobalStateExplorers/FLEXCookiesTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  FLEXCookiesTableViewController.h
+//  FLEX
+//
+//  Created by Rich Robinson on 19/10/2015.
+//  Copyright © 2015 Flipboard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FLEXCookiesTableViewController : UITableViewController
+
+@end
