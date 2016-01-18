9b1318e975cb38de401974de8c2d963c3871baff
Ryan Olson <ryanolsonk@gmail.com>
2016-01-17 20:52:34 -0800

    Project structure cleanup

--- Classes/FLEX.h
+++ Classes/FLEX.h
@@ -6,12 +6,4 @@
 //  Copyright (c) 2015 Flipboard. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
-
-//! Project version number for FLEX.
-FOUNDATION_EXPORT double FLEXVersionNumber;
-
-//! Project version string for FLEX.
-FOUNDATION_EXPORT const unsigned char FLEXVersionString[];
-
 #import <FLEX/FLEXManager.h>
