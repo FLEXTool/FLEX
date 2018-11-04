4ec7029334e4795ba2035e67629ce7f8f98da846
Tanner Bennett <tannerbennett@me.com>
2018-11-04 02:24:05 -0600

    Detect and unbox pointers to objects from void *
    - Also unbox C strings into NSString
    - Also adds return type encoding string to method calling view controller

--- Classes/Editing/FLEXMethodCallingViewController.m
+++ Classes/Editing/FLEXMethodCallingViewController.m
@@ -15,6 +15,7 @@
 @interface FLEXMethodCallingViewController ()
 
 @property (nonatomic, assign) Method method;
+@property (nonatomic, assign) FLEXTypeEncoding *returnType;
 
 @end
 
@@ -25,7 +26,8 @@
     self = [super initWithTarget:target];
     if (self) {
         self.method = method;
-        self.title = [self isClassMethod] ? @"Class Method" : @"Method";
+        self.returnType = [FLEXRuntimeUtility returnTypeForMethod:method];
+        self.title = [self isClassMethod] ? @"Class Method" : @"Method";;
     }
     return self;
 }
@@ -34,7 +36,11 @@
 {
     [super viewDidLoad];
     
-    self.fieldEditorView.fieldDescription = [FLEXRuntimeUtility prettyNameForMethod:self.method isClassMethod:[self isClassMethod]];
+    NSString *returnType = @((const char *)self.returnType);
+    NSString *methodDescription = [FLEXRuntimeUtility prettyNameForMethod:self.method isClassMethod:[self isClassMethod]];
+    NSString *format = @"Signature:\n%@\n\nReturn Type:\n%@";
+    NSString *info = [NSString stringWithFormat:format, methodDescription, returnType];
+    self.fieldEditorView.fieldDescription = info;
     
     NSArray<NSString *> *methodComponents = [FLEXRuntimeUtility prettyArgumentComponentsForMethod:self.method];
     NSMutableArray<FLEXArgumentInputView *> *argumentInputViews = [NSMutableArray array];
@@ -50,6 +56,12 @@
         argumentIndex++;
     }
     self.fieldEditorView.argumentInputViews = argumentInputViews;
+}
+
+- (void)dealloc
+{
+    free(self.returnType);
+    self.returnType = NULL;
 }
 
 - (BOOL)isClassMethod
@@ -84,6 +96,11 @@
         NSString *message = [error localizedDescription];
         UIAlertView *alert = [[UIAlertView alloc] initWithTitle:title message:message delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil];
         [alert show];
+    } else if (returnedObject) {
+        // For non-nil (or void) return types, push an explorer view controller to display the returned object
+        returnedObject = [FLEXRuntimeUtility potentiallyUnwrapBoxedPointer:returnedObject type:self.returnType];
+        FLEXObjectExplorerViewController *explorerViewController = [FLEXObjectExplorerFactory explorerViewControllerForObject:returnedObject];
+        [self.navigationController pushViewController:explorerViewController animated:YES];
     } else {
         [self exploreObjectOrPopViewController:returnedObject];
     }
