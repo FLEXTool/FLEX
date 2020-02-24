c661d491a5bf4926be7ede6f9170d74a5a10515f
Tanner Bennett <tannerbennett@me.com>
2020-02-24 17:36:34 -0600

    Use SF Mono / Menlo for log font

--- Classes/GlobalStateExplorers/SystemLog/FLEXSystemLogCell.m
+++ Classes/GlobalStateExplorers/SystemLog/FLEXSystemLogCell.m
@@ -8,6 +8,7 @@
 
 #import "FLEXSystemLogCell.h"
 #import "FLEXSystemLogMessage.h"
+#import "UIFont+FLEX.h"
 
 NSString *const kFLEXSystemLogCellIdentifier = @"FLEXSystemLogCellIdentifier";
 
@@ -69,7 +70,7 @@ static const UIEdgeInsets kFLEXLogMessageCellInsets = {10.0, 10.0, 10.0, 10.0};
 
 + (NSAttributedString *)attributedTextForLogMessage:(FLEXSystemLogMessage *)logMessage highlightedText:(NSString *)highlightedText {
     NSString *text = [self displayedTextForLogMessage:logMessage];
-    NSDictionary<NSString *, id> *attributes = @{ NSFontAttributeName : [UIFont fontWithName:@"CourierNewPSMT" size:12.0] };
+    NSDictionary<NSString *, id> *attributes = @{ NSFontAttributeName : UIFont.flex_codeFont };
     NSAttributedString *attributedText = [[NSAttributedString alloc] initWithString:text attributes:attributes];
 
     if (highlightedText.length > 0) {
