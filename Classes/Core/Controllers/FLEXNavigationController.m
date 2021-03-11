fad038392b94dd773a1ae02993c800e3089698d4
Alexey Salangin <22199708+magauran@users.noreply.github.com>
2021-03-11 22:45:23 +0300

    Don't show toolbar when tapping on a nav bar button (#517)

--- Classes/Core/Controllers/FLEXNavigationController.m
+++ Classes/Core/Controllers/FLEXNavigationController.m
@@ -148,6 +148,13 @@
 }
      
 - (void)handleNavigationBarTap:(UIGestureRecognizer *)sender {
+    // Don't reveal the toolbar if we were just tapping a button
+    CGPoint location = [sender locationInView:self.navigationBar];
+    UIView *hitView = [self.navigationBar hitTest:location withEvent:nil];
+    if ([hitView isKindOfClass:[UIControl class]]) {
+        return;
+    }
+
     if (sender.state == UIGestureRecognizerStateRecognized) {
         if (self.toolbarHidden) {
             [self setToolbarHidden:NO animated:YES];
