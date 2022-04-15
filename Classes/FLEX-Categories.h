9042bc9dc80af9e03dcaaf965b777c11febb06d8
Canius Chu <canius.chu@farfetch.com>
2022-04-15 13:51:19 +0800

    fix import headers

--- Classes/FLEX-Categories.h
+++ Classes/FLEX-Categories.h
@@ -6,17 +6,15 @@
 //  Copyright © 2020 FLEX Team. All rights reserved.
 //
 
+#import "UIBarButtonItem+FLEX.h"
+#import "CALayer+FLEX.h"
+#import "UIFont+FLEX.h"
+#import "UIGestureRecognizer+Blocks.h"
+#import "UIPasteboard+FLEX.h"
+#import "UIMenu+FLEX.h"
+#import "UITextField+Range.h"
 
-
-#import <UIBarButtonItem+FLEX.h>
-#import <CALayer+FLEX.h>
-#import <UIFont+FLEX.h>
-#import <UIGestureRecognizer+Blocks.h>
-#import <UIPasteboard+FLEX.h>
-#import <UIMenu+FLEX.h>
-#import <UITextField+Range.h>
-
-#import <NSObject+FLEX_Reflection.h>
-#import <NSArray+FLEX.h>
-#import <NSUserDefaults+FLEX.h>
-#import <NSTimer+FLEX.h>
+#import "NSObject+FLEX_Reflection.h"
+#import "NSArray+FLEX.h"
+#import "NSUserDefaults+FLEX.h"
+#import "NSTimer+FLEX.h"
