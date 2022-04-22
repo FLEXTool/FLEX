ba9cb43b6af792791a80874b8fa1337eca66e1b7
Canius Chu <canius.chu@farfetch.com>
2022-04-23 01:18:35 +0800

    Fix compile errors and warnings (#597)
    * Update INSTALL_PATH
    * Update run path
    * Add FLEXFirebaseTransaction.mm into project which break in commit: d2e0db87731fd16c90462625a8fdfad642611b00
    * Fix header imports
    * Rollback LD_RUNPATH_SEARCH_PATHS

--- Classes/FLEX.h
+++ Classes/FLEX.h
@@ -7,19 +7,19 @@
 //  Copyright (c) 2020 FLEX Team. All rights reserved.
 //
 
-#import <FLEXManager.h>
-#import <FLEXManager+Extensibility.h>
-#import <FLEXManager+Networking.h>
+#import "FLEXManager.h"
+#import "FLEXManager+Extensibility.h"
+#import "FLEXManager+Networking.h"
 
-#import <FLEXExplorerToolbar.h>
-#import <FLEXExplorerToolbarItem.h>
-#import <FLEXGlobalsEntry.h>
+#import "FLEXExplorerToolbar.h"
+#import "FLEXExplorerToolbarItem.h"
+#import "FLEXGlobalsEntry.h"
 
-#import <FLEX-Core.h>
-#import <FLEX-Runtime.h>
-#import <FLEX-Categories.h>
-#import <FLEX-ObjectExploring.h>
+#import "FLEX-Core.h"
+#import "FLEX-Runtime.h"
+#import "FLEX-Categories.h"
+#import "FLEX-ObjectExploring.h"
 
-#import <FLEXMacros.h>
-#import <FLEXAlert.h>
-#import <FLEXResources.h>
+#import "FLEXMacros.h"
+#import "FLEXAlert.h"
+#import "FLEXResources.h"
